d6d277a961b3207eae6310c5b6b8f524e9de6f47
mccdo@iastate.edu <mccdo@iastate.edu@14f10451-d5f6-a1d5-eb13-46750b702d30>
2013-02-07 21:10:57 +0000

    update debug output

--- trunk/src/crunchstore/Multiplexer.cxx
+++ trunk/src/crunchstore/Multiplexer.cxx
@@ -92,7 +92,8 @@ void Multiplexer::Load( Persistable& persistable, Role role )
     }
     case BACKING_ROLE:
     {
-        std::cout << "Multiplexer::Load: Falling back to the BACKING_ROLE." << std::endl;
+        std::cout << "Multiplexer::Load: Falling back to the BACKING_ROLE for persistable "
+            << persistable.GetTypeName() << std::endl;
         // Find out which store contains this persistable and load from there.
         // TODO: Should we be caching locations of IDs on load?
         const boost::uuids::uuid id = persistable.GetUUID();
