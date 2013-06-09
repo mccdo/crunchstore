ae8b5aad90b893a8de351750c04bc4faab1f3829
doug.mccorkle@gmail.com <doug.mccorkle@gmail.com@14f10451-d5f6-a1d5-eb13-46750b702d30>
2013-06-09 02:59:42 +0000

    correct for poco 1.5.1 errors when there is header pollution.

--- trunk/src/crunchstore/SQLiteStore.h
+++ trunk/src/crunchstore/SQLiteStore.h
@@ -23,6 +23,8 @@
 #include <crunchstore/SQLiteStorePtr.h>
 #include <crunchstore/Logging.h>
 #include <crunchstore/TransactionKey.h>
+//This is a hack to get around an issue with POCO 1.5.1 or greater
+using namespace std;
 #include <crunchstore/SQLiteTransactionKey.h>
 
 #include <Poco/Data/SQLite/SQLite.h>
